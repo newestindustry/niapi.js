04b69283ec489298e8cf52e8c92619fe657a95ff
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2013-12-02 14:54:30 +0100

    Added sidebar and messages functionality

--- niapisdk.js
+++ niapisdk.js
@@ -3,87 +3,89 @@ window.NI = (function () {
     var NI = {
         api_url: "https://api.newestindustry.nl",
         auth_url: "/oauth/auth/",
-        client_id:false,
-        redirect_uri:false,
-        scope:"default",
-        oauth_token:false,
+        client_id: false,
+        redirect_uri: false,
+        scope: "default",
+        oauth_token: false,
         refresh_token: false,
-        error:false,
-        me:false,
+        error: false,
+        me: false,
+        root: document.getElementById('ni-root'),
 
         init: function(obj) {
             if(typeof obj !== "undefined") {
-                this.api_url        = (typeof obj.api_url !== "undefined") ? obj.api_url : this.api_url;
-                this.client_id      = (typeof obj.client_id !== "undefined") ? obj.client_id : false;
-                this.redirect_uri   = (typeof obj.redirect_uri !== "undefined") ? obj.redirect_uri : false;
+                NI.api_url        = (typeof obj.api_url !== "undefined") ? obj.api_url : NI.api_url;
+                NI.client_id      = (typeof obj.client_id !== "undefined") ? obj.client_id : false;
+                NI.redirect_uri   = (typeof obj.redirect_uri !== "undefined") ? obj.redirect_uri : false;
             }
         
-            this.parseToken();
+            NI.parseToken();
         },
         
-        emptyCallback: function(error, data) {
+        emptyCallback: function() {
             
         },
         
         parseToken: function() {
-            var params = {}, queryString = location.hash.substring(1), regex = /([^&=]+)=([^&]*)/g, m;
-            while (m = regex.exec(queryString)) {
-                params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+            var params = {}, queryString = location.hash.substring(1), regex = /([^&=]+)=([^&]*)/g;
+            var queryStringParts = regex.exec(queryString);
+            if(queryStringParts) {
+                params[decodeURIComponent(queryStringParts[1])] = decodeURIComponent(queryStringParts[2]);
             }
-            
+
             if(params.token) {
-                this.oauth_token = params.token;
-                localStorage.oauth_token = this.oauth_token;
+                NI.oauth_token = params.token;
+                localStorage.oauth_token = NI.oauth_token;
             } else if(localStorage.oauth_token) {
-                this.oauth_token = localStorage.oauth_token;
+                NI.oauth_token = localStorage.oauth_token;
             }
             
             if(params.error) {
-                this.error = params.error;
+                NI.error = params.error;
             }
         },
     
         Me: function(callback) {
-            this.Get("/me", callback);
+            NI.Get("/me", callback);
         },
     
         Login: function() {
-            if(!this.oauth_token) {
-                location.href = this.api_url+this.auth_url+"?response_type=token&client_id="+this.client_id+"&redirect_uri="+this.redirect_uri+"&scope="+this.scope;
+            if(!NI.oauth_token) {
+                location.href = NI.api_url+NI.auth_url+"?response_type=token&client_id="+NI.client_id+"&redirect_uri="+NI.redirect_uri+"&scope="+NI.scope;
             }
         },
         
         Logout: function() {
-            if(this.oauth_token) {
-                this.Delete("/oauth/token", this.emptyCallback);    
+            if(NI.oauth_token) {
+                NI.Delete("/oauth/token", NI.emptyCallback);
             }
             
-            this.oauth_token = false;
+            NI.oauth_token = false;
             localStorage.removeItem('oauth_token');
         },
         
         Get: function(uri, callback) {
-            this.call(this.api_url+uri, callback, "GET");
+            NI.call(NI.api_url+uri, callback, "GET");
         },
         
         Post: function(uri, callback, data) {
             if(typeof data === "object") {
-                data = this.toQueryString(data);
+                data = NI.toQueryString(data);
             }
 
-            this.call(this.api_url+uri, callback, "POST", data);
+            NI.call(NI.api_url+uri, callback, "POST", data);
         },
         
         Put: function(uri, callback, data) {
             if(typeof data === "object") {
-                data = this.toQueryString(data);
+                data = NI.toQueryString(data);
             }
 
-            this.call(this.api_url+uri, callback, "PUT", data);
+            NI.call(NI.api_url+uri, callback, "PUT", data);
         },
         
         Delete: function(uri, callback) {
-            this.call(this.api_url+uri, callback, "DELETE");
+            NI.call(NI.api_url+uri, callback, "DELETE");
         },
         
         toQueryString: function(obj) {
@@ -95,6 +97,28 @@ window.NI = (function () {
                 seperator = "&";
             }
             return str;
+        },
+        
+        loadSidebar: function() {
+            NI.call(NI.api_url+"/frontend/menu/left/_format/html", function(error, data) {
+                document.getElementById('ni-root').innerHTML = data;
+                
+                NI.call(NI.api_url+"/mail/messages/thread/false/read/false/_limit/1", function(error, data) {
+                    document.getElementById('ni-totalUnreadMessages').innerHTML = data.total;
+                });
+            });
+        },
+        
+        getMessages: function() {
+            NI.call(NI.api_url+"/mail/messages/thread/false/_limit/5/_sort/created/_direction/desc/_format/html", function(error, data) {
+                document.getElementById('ni-messagesPreviewList').innerHTML = data;
+                if(document.getElementById('ni-messagesPreview').style.display === "none") {
+                    document.getElementById('ni-messagesPreview').style.display = "block";
+                } else {
+                    document.getElementById('ni-messagesPreview').style.display = "none";
+                }
+                
+            });
         },
         
         call: function(url, callback, method, post) {
@@ -131,8 +155,15 @@ window.NI = (function () {
                     } else {
                         error = new Error("NIApi: Response status: "+x.status);
                     }
+
+                    var data;
                     
-                    var data = JSON.parse(x.responseText);
+                    if(x.getResponseHeader('content-type').indexOf("application/json") !== -1) {
+                        data = JSON.parse(x.responseText);
+                    } else {
+                        data = x.responseText;
+                    }
+                    
                     callback(error, data, x);
                 }
             };
@@ -145,8 +176,8 @@ window.NI = (function () {
         
             x.open(requestMethod, url+"/_ts/" + Date.now(), true);
         
-            if(this.oauth_token) {
-                x.setRequestHeader("Authorization", "oauth_token "+this.oauth_token);
+            if(NI.oauth_token) {
+                x.setRequestHeader("Authorization", "oauth_token "+NI.oauth_token);
             }
         
             if(!post) {
